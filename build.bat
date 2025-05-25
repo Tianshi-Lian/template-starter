6fb7823ae4f6808c50971b13258d51c94c683f5a
randy <tom@randy.gg>
2025-05-25 15:36:07 +0700

    moved build into /bald

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @echo off
 
 rem This package is a build script, see build.odin for more
-odin run sauce\build -debug -collection:bald=sauce/bald -- testarg
+odin run sauce\bald\build -debug -collection:bald=sauce/bald -- testarg
